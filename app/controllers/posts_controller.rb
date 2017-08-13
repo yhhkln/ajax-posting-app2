71436144568501bf5a6ec829d4d345d4741f2e33
杨昊霖 <yanghaolin@yanghaolindeMacBook-Pro.local>
2017-08-13 23:31:50 +0800

    ajax like unlike

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,8 +22,6 @@ class PostsController < ApplicationController
 		unless @post.find_like(current_user)
 			Like.create( :user => current_user, :post => @post)
 		end
-
-		redirect_to posts_path
 	end
 
 	def unlike
@@ -31,7 +29,7 @@ class PostsController < ApplicationController
 		like = @post.find_like(current_user)
 		like.destroy
 
-		redirect_to posts_path
+		render "like"
 	end
 
 	protected
